0caacbc290430a38af8d320fde8fe29540f4634a
KeekaYagnash <yagnash.keeka@disraptor.co.za>
2024-07-03 13:34:30 +0200

    test lint

--- main.tf
+++ main.tf
@@ -223,16 +223,16 @@ resource "aws_s3_bucket_notification" "example" {
 
 # Invoking lambda function
 resource "aws_lambda_permission" "s3_invoke_permission" {
-  statement_id  = "AllowS3Invocation"
-  action        = "lambda:InvokeFunction"
-  function_name = aws_lambda_function.lambda_code.function_name
-  principal     = "s3.amazonaws.com"
-  source_arn    = data.aws_s3_bucket.vector_bucket.arn
+  statement_id = "AllowS3Invocation"
+  action       = "lambda:InvokeFunction"
+  #   function_name = aws_lambda_function.lambda_code.function_name
+  #   principal     = "s3.amazonaws.com"
+  #   source_arn    = data.aws_s3_bucket.vector_bucket.arn
 }
 
 # Cloudwatch Policy
 resource "aws_iam_policy" "lambda_cloudwatch_logs_policy" {
-  #   name        = "LambdaCloudWatchLogsPolicy"
+  name        = "LambdaCloudWatchLogsPolicy"
   description = "Policy for Lambda to write errors to CloudWatch Logs"
   tags = {
     name        = "ai-shop-s3-cloudwatch-policy"
