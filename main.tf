e83fe9c3dab34aae3007f01ba4636f18670eb561
KeekaYagnash <yagnash.keeka@disraptor.co.za>
2024-07-04 09:15:56 +0200

    new lint

--- main.tf
+++ main.tf
@@ -61,7 +61,7 @@ resource "aws_s3_bucket_object" "object_upload2" {
   key    = "lambda_function.zip"
   source = "./lambda/lambda_function.zip"
 }
-
+  
 resource "aws_s3_bucket_policy" "prompt_bucket_policy" {
   bucket = aws_s3_bucket.load-path-and-userid_upload.bucket
   policy = jsonencode({
